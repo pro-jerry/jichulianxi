d726906671eca4bbf0ef9d2008c812e433a5e3aa
Administrator <Administrator@ROXATL7G3T4SVPD>
2015-12-09 18:38:11 +0800

    Callable

--- src/com/hj/thread/create/Call.java
+++ src/com/hj/thread/create/Call.java
@@ -16,14 +16,24 @@ public class Call {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		
 		//创建对象
-		ExecutorService ser = Executors.newFixedThreadPool(1);
+		ExecutorService ser = Executors.newFixedThreadPool(2);
 		
-		Race tortoise = new Race();
+		Race tortoise = new Race("wugu",1000);
+		Race rabit = new Race("兔子",100);
 		//获取值
-		Future<Integer> result = ser.submit(tortoise);
-		int num =  result.get();
+		Future<Integer> result1 = ser.submit(tortoise);
+		Future<Integer> result2 = ser.submit(rabit);
 		
-		System.out.println(num);
+		Thread.sleep(2000);
+		
+		tortoise.setFlag(false);
+		rabit.setFlag(false);
+		
+		
+		int num1 =  result1.get();
+		int num2 =  result2.get();
+		System.out.println(num1);
+		System.out.println(num2);
 		//停止服务
 		ser.shutdownNow();
 	}
@@ -31,10 +41,63 @@ public class Call {
 
 class Race implements Callable<Integer>{
 
+	private String name; //名称
+	private long time;	//延时时间
+	private boolean flag=true;
+	private int step=0;//步
+	
+	public Race() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Race(String name,long time) {
+		super();
+		this.name = name;
+		this.time = time;
+	}
+
 	@Override
 	public Integer call() throws Exception {
 
-		return 1000;
+		while(flag){
+			
+			Thread.sleep(time);
+			step++;
+		}
+		return step;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+	public int getStep() {
+		return step;
+	}
+
+	public void setStep(int step) {
+		this.step = step;
 	}
 	
 	
