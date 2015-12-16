cf7a6ae35744d9dc26f67d1f465c60015789299d
Administrator <Administrator@ROXATL7G3T4SVPD>
2015-12-16 17:50:48 +0800

    server

--- src/com/hj/socket/chat/Server.java
+++ src/com/hj/socket/chat/Server.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 服务器
@@ -13,20 +15,114 @@ import java.net.Socket;
  */
 public class Server {
 
+	
+	private List<MyChannel> all = new ArrayList<MyChannel>();
+	
 	public static void main(String[] args) throws IOException {
 		
+		new Server().start();
+	}
+	
+	public void start() throws IOException{
+		
 		ServerSocket server = new ServerSocket(9999);
-		Socket client =  server.accept();
-		//写出数据
-		//输入流
-		DataInputStream dis = new DataInputStream(client.getInputStream());
-		DataOutputStream dos = new DataOutputStream(client.getOutputStream());
 		while(true){
-			String msg = dis.readUTF();
-			System.out.println(msg);
-			//输出流
-			dos.writeUTF("服务器--->:"+msg);
-			dos.flush();
+			
+			Socket client = server.accept();
+			MyChannel channel = new MyChannel(client);
+			all.add(channel);
+			new Thread(channel).start();//一条道路
 		}
 	}
+	
+	
+	
+	/**
+	 * 一个客户端一条道路
+	 * 输入流
+	 * 输出流
+	 * 接收数据
+	 * 发送数据
+	 * @author Administrator
+	 *
+	 */
+	private class MyChannel implements Runnable{
+
+		private DataInputStream dis;
+		private DataOutputStream dos;
+		private boolean isRunning = true;
+		
+		public MyChannel(Socket client ) {
+			try {
+				dis = new DataInputStream(client.getInputStream());
+				dos = new DataOutputStream(client.getOutputStream());
+			} catch (IOException e) {
+
+				CloseUtil.closeAll(dis,dos);
+				isRunning = false;
+			}
+		}
+		
+		/**
+		 * 读取数据
+		 * @return
+		 */
+		private String receive(){
+			
+			String msg = "";
+			try {
+				msg = dis.readUTF();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return msg;
+		}
+
+		
+		private void send(String msg){
+			
+			if(msg==null || msg.equals("")){
+				return ;
+			}
+			try {
+				dos.writeUTF(msg);
+				dos.flush();
+			} catch (IOException e) {
+				CloseUtil.closeAll(dos);
+				isRunning = false;
+				all.remove(this);//移出自身
+			}
+			
+		}
+		
+		/**
+		 * 发送给其他客户端
+		 */
+		private void sendOthers(){
+			
+			String msg = this.receive();
+			//遍历容器
+			for(MyChannel other:all){
+				
+				if(other == this){
+					continue;
+				}
+				//发送给其他客户端
+				other.send(msg);
+			}
+		}
+		
+		
+		@Override
+		public void run() {
+
+			while(isRunning){
+				
+//				send(receive());
+				sendOthers();
+			}
+		}
+		
+	}
+	
 }
