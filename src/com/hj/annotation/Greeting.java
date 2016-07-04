5bd44b6fd2793ba089ea8b8109787e7703e8d57b
军 <@UBUNTU>
2016-07-04 10:24:32 +0800

    1

---
+++ src/com/hj/annotation/Greeting.java
@@ -0,0 +1,12 @@
+package com.hj.annotation;
+
+import java.lang.annotation.Inherited;
+
+@Inherited
+public @interface Greeting {
+
+	public enum FontColor{BULE,RED,GREEN};
+	String name();
+	FontColor fontColor() default FontColor.RED;
+	
+}
---
+++ src/com/hj/annotation/Table.java
@@ -0,0 +1,12 @@
+package com.hj.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface Table {
+
+	public String tableName() default "className";
+}
+
+
